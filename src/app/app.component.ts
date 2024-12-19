5f3adb71ec28bfb4143b64790ef9c29096f5045b
aking1999 <aleksa.aking.1999@gmail.com>
2024-12-19 09:25:43 +0100

    Added initial navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navigation/navbar/navbar.component';
+import { SidebarComponent } from './navigation/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, SidebarComponent, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'PanelPro';

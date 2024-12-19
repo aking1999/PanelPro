53484c3a3eca736dd8fa8190154580fd6f8cd403
aking1999 <aleksa.aking.1999@gmail.com>
2024-12-19 12:17:15 +0100

    Added routing for sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,33 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    /*{
-        path: '',
-
-    }*/
+  {
+    path: '',
+    loadComponent: () =>
+      import('./dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent
+      ),
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    redirectTo: '/',
+  },
+  {
+    path: 'orders',
+    loadComponent: () =>
+      import('./orders/orders.component').then((m) => m.OrdersComponent),
+  },
+  {
+    path: 'servers',
+    loadComponent: () =>
+      import('./servers/servers.component').then((m) => m.ServersComponent),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./not-found/not-found.component').then(
+        (m) => m.NotFoundComponent
+      ),
+  },
 ];
